4791853e9cd5948c8883e6a1101099774a2ba8e4
jarkynbek <jarkynbekbat@gmail.com>
2020-06-09 21:24:17 +0600

    color theme is changed

--- lib/app_config.dart
+++ lib/app_config.dart
@@ -11,12 +11,9 @@ import 'ui/screens/profile/profile_screen.dart';
 
 ThemeData getTheme() {
   return ThemeData(
-    primaryColor: Color(0xff45046a),
-    // primaryColor: Color(0xff151721),
-    backgroundColor: Color(0xff5c2a9d),
+    primaryColor: Color(0xff342b38),
     accentColor: Color(0xffff0000),
-
-    scaffoldBackgroundColor: Color(0xff5c2a9d),
+    scaffoldBackgroundColor: Color(0xff151721),
     textTheme: TextTheme(
       bodyText1: TextStyle(
         color: Colors.white.withOpacity(0.7),
